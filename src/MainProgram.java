72e679339d0b0c91ed19e68e4a9a662882c16b7e
Brock Tubre <brocktubre@gmail.com>
2014-12-17 00:41:08 -0600

    Added some comments

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,3 +1,14 @@
+/**
+ * 	Author: 	Brock Tubre
+ * 	Date: 		December 19, 2014
+ * 	CWID:		102-10-73
+ * 	Assignment: 1
+ * 	File path: 	/Volumes/Partition512/School/CSC470/Assignment1
+ * 
+ * 	Description: This program projects a 3D pyramid that renders a wireframe. It allows the user to scale the obejct using SHIFT up and SHIFT down.
+ * 	The program runs in Eclipse Version: Luna Service Release 1 (4.4.1). 
+ */
+
 import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Color;
@@ -8,16 +19,19 @@ public class MainProgram extends JFrame{
 
 	private static final long serialVersionUID = 1L;
 	
+	// Initials the canvas object
 	private MyCanvas canvas = new MyCanvas();
 	
-	int x = 800, y = 700;
+	// Intializes the canvas size and also the origin of the canvas. 
+	int x = 1024, y = 500;
 	int x_origin = x/2, y_origin = y/2;
 	
-	
+	// main program
 	public static void main(String[] args){
 		MainProgram p = new MainProgram();
 	}
 	
+	// main program function that declare the canvas attributes
 	public MainProgram(){
 		
 		setLayout(new BorderLayout());
@@ -33,13 +47,15 @@ public class MainProgram extends JFrame{
 	}
 	
 
-	
+	// the MyCanvas class extends canvas and lets us draw onto the canvas
 	private class MyCanvas extends Canvas{
 		
 		private static final long serialVersionUID = 1L;
 
 		@Override
 		public void paint(Graphics g){
+			
+			// the points of the polygon, a 2D array 
 			int[][] polygon_points = new int[5][3];
 
 			// P1
@@ -67,10 +83,12 @@ public class MainProgram extends JFrame{
 			polygon_points[4][1] = y_origin + 25;
 			polygon_points[4][2] = 0;
 			
+			// Draws the X and Y corrordinants. 
 			g.setColor(Color.CYAN);
 			g.drawLine(x/2, 0, x/2, y);
 			g.drawLine(0, y/2, x, y/2);
 			
+			// Draws the 3D triangle
 			g.setColor(Color.WHITE);
 			g.drawLine(polygon_points[0][0], polygon_points[0][1], polygon_points[1][0], polygon_points[1][1]); // P1 to P2
 			g.drawLine(polygon_points[2][0], polygon_points[2][1], polygon_points[0][0], polygon_points[0][1]); // P3 to P1
